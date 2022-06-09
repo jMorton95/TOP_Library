fbd70047525f0bc0ed52d3b7168f7e979ad0e50f
unknown <mortson@hotmail.co.uk>
2022-06-09 22:42:05 +0100

    Updated Readme, added JavaScript DOM Card Creation functionality, adjusted Object Storage patterns

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # TOP_Library
 Library Project from The Odin Project Cirriculum
 
+To Do:
+    --Main Features--
+-Update Button
+-Remove Book Button
+
+    --Minor Features--
+-Card Text Wrapping
+-Add page button animation for more responsiveness
+
 Demo: https://klavelon.github.io/TOP_Library/
--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-let myLibrary = [];
-//Default Library entry to display on the page.
-myLibrary[0] = new book("George R.R Martin", "A Song of Ice and Fire", 750, true);
 
 //Object constructor for our library.
-function book(author, title, pages, read = false) {
+function book(author, title, pages, read) {
     this.author = author;
     this.title = title;
     this.pages = pages;
     this.read = read;
+
+    this.readStatus = function(){
+        if (read == true){
+            read == false;
+        } else {
+            read == true;
+        } 
+        this.classList.toggle("readTrue");
+    }
 }
 
- //Return the status of our Checkbox.
-function isChecked() {
+//Return the status of our Checkbox.
+function isChecked(){
     let x = document.getElementById('readCheck');
     if ((x.checked) == true) {
         return true;
-    }   else    {
+    } else {
         return false;
     }
 }
@@ -29,7 +35,7 @@ function resetForm(){
     formItems[3].checked = false;
 }
 
-function newBook() {
+function storeBook(){
     //Declare temporary variables to pass as constructor parameters from our form.
     let a = document.getElementById('authorBot').value;
     let t = document.getElementById('titleBot').value;
@@ -37,10 +43,9 @@ function newBook() {
     let r = isChecked();
     //Create new Book object from our form, store it in our library.
     myLibrary[myLibrary.length] = new book(a, t, p, r);
-    //Reset our form.
-    resetForm();
-    //Hide our Form.
-    document.getElementById("addBookForm").style.display = "none";
+
+    //Reset and hide our form, create a card from new Object.
+    resetForm(); closeForm(); createCard();
 }
 
 function openAddForm(){
@@ -51,51 +56,60 @@ function closeForm(){
     document.getElementById("addBookForm").style.display = "none";
 }
 
-function createBooks() {
-    let target = document.querySelector(".container");
-    for (x = myLibrary.length; x > 1; x--){
+function createCard(){
+    let cardContainer = document.querySelector(".container");
 
-        let newBook = document.createElement("div");
-        newBook.classList.add("bookCard");
-        let newAuthor = document.createElement("p");
-        newAuthor.classList.add("author");
-        let newTitle = document.createElement("p");
-        newTitle.classList.add("title")
-        let newMidSection = document.createElement("div");
-        newMidSection.classList.add("midSection");
-        let newPages = document.createElement("p");
-        newPages.classList.add("pages");
-        let newRead = document.createElement("div");
-        newRead.classList.add("read");
-        let newUpdate = document.createElement("div");
-        newUpdate.classList.add("update");
+    let newBook = document.createElement("div");
+    let newAuthor = document.createElement("p");
+    let newTitle = document.createElement("p");
+    let newMidSection = document.createElement("div");
+    let newPages = document.createElement("p");
+    let newRead = document.createElement("div");
+    let newUpdate = document.createElement("div");
+        
+    newBook.classList.add("bookCard");
+    newAuthor.classList.add("author");
+    newTitle.classList.add("title");
+    newMidSection.classList.add("midSection");
+    newPages.classList.add("pages");
+    newRead.classList.add("read");
+    newUpdate.classList.add("update");
 
-        target.appendChild(newBook);
-        newBook.append(newAuthor, newTitle, newMidSection, newUpdate);
-        newMidSection.append(newPages, newRead);
+    cardContainer.appendChild(newBook);
+    newBook.append(newAuthor, newTitle, newMidSection, newUpdate);
+    newMidSection.append(newPages, newRead);
+
+    newRead.textContent = "Read Y/N?";
+    newUpdate.textContent = "Update";
+    newAuthor.textContent = myLibrary[numOfCards].author;
+    newTitle.textContent = myLibrary[numOfCards].title;
+    newPages.textContent = myLibrary[numOfCards].pages;
+    if (myLibrary[numOfCards].read == true){
+        newRead.classList.add("readTrue");
     }
+    numOfCards++;
+    
 }
 
+let myLibrary = [];
+let numOfCards = 0;
+//Default Library entry to display on the page.
+myLibrary[0] = new book("George R.R Martin", "A Song of Ice and Fire", 750, true);
 
 
+createCard();
 
-createBooks();
-
-document.querySelector(".author").textContent = myLibrary[0].author;
-document.querySelector(".title").textContent = myLibrary[0].title;
-document.querySelector(".pages").textContent = myLibrary[0].pages;
-
-//Anonymous function that adjusts default CSS based on if a book has been read or not.
-(() => {
+//Deprecated Anonymous function that adjusted default CSS based on if a book has been read or not.
+/*(() => {
     for (i = myLibrary.length - 1; i > -1; i--){
     if (myLibrary[i].read === true) {
         document.querySelector(".read").classList.add('readTrue');
     };}
-})();
+})();*/
 
 const readStatus = document.querySelectorAll(".read");  
 
 readStatus.forEach(readStatus => {
     readStatus.addEventListener('click', () => {
          readStatus.classList.toggle('readTrue'); }
-    )});
+)});
