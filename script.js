186f19d1013a292cb85565add595894b741d680f
unknown <mortson@hotmail.co.uk>
2022-06-13 14:16:24 +0100

    Cleaned up code for readability, completed updateLoad function to pull up existing Book entries for editing

--- script.js
+++ script.js
@@ -1,134 +1,168 @@
 let myLibrary = [];
 let numOfCards = 0;
-
-let updateIndex = function(title){
-    return myLibrary.findIndex(obj =>{
-        return obj.title === title;
-    })
-}
+let indexNumber;
 
 //Default Library entry to display on the page.
-myLibrary[0] = new book("George R.R Martin", "A Song of Ice and Fire", 750, false);
+myLibrary[0] = new book(
+  "George R.R Martin",
+  "A Song of Ice and Fire",
+  750,
+  true
+);
 
 //Object constructor for our library.
 function book(author, title, pages, read) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
 
-    this.readStatus = function(){
-        if (read == true){
-            read == false;
-        } else {
-            read == true;
-        } 
-        this.classList.toggle("readTrue");
+  this.readStatus = function () {
+    if (read == true) {
+      read == false;
+    } else {
+      read == true;
     }
+    this.classList.toggle("readTrue");
+  };
 }
 
-function openAddForm(){ document.getElementById("addBookForm").style.display = "block"; }
-function closeForm(){ document.getElementById("addBookForm").style.display = "none"; }
+function openAddForm() {
+  document.getElementById("addBookForm").style.display = "block";
+  document.getElementById("add").textContent = "Add";
+}
+
+function closeForm() {
+  document.getElementById("addBookForm").style.display = "none";
+  resetForm();
+}
 
 //Select all our form elements, remove their values and specifically uncheck the checkbox.
-function resetForm(){
-    const formItems = document.querySelectorAll('.formItem');
-    formItems.forEach(formItem => {
-        formItem.value = "";
-    });
-    formItems[3].checked = false;
+function resetForm() {
+  const formItems = document.querySelectorAll(".formItem");
+  formItems.forEach((formItem) => {
+    formItem.value = "";
+  });
+  formItems[3].checked = false;
 }
 
 //Return the status of our Checkbox.
-function isChecked(){
-    let checkbox = document.getElementById('readCheck');
-    if ((checkbox.checked) == true) {
-        return true;
-    } else {
-        return false;
-    }
+function isChecked() {
+  let checkbox = document.getElementById("readCheck");
+  if (checkbox.checked == true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //Create Book Objects from Inputs
-function storeBook(author, title, pages, read){
-    myLibrary[myLibrary.length] = new book(author, title, pages, read);
+function storeBook(author, title, pages, read) {
+  myLibrary[myLibrary.length] = new book(author, title, pages, read);
 
-    //Reset and hide our form, create a card from new Object.
-    resetForm(); closeForm(); createCard();
+  //Reset and hide our form, create a card from new Object.
+  resetForm();
+  closeForm();
+  createCard();
 }
 
-function createCard(){
-    let cardContainer = document.querySelector(".container");
+function createCard() {
+  let cardContainer = document.querySelector(".container");
 
-    let newBook = document.createElement("div");
-    let newAuthor = document.createElement("p");
-    let newTitle = document.createElement("p");
-    let newMidSection = document.createElement("div");
-    let newPages = document.createElement("p");
-    let newRead = document.createElement("div");
-    let newUpdate = document.createElement("div");
-        
-    newBook.classList.add("bookCard");
-    newAuthor.classList.add("author");
-    newTitle.classList.add("title");
-    newMidSection.classList.add("midSection");
-    newPages.classList.add("pages");
-    newRead.classList.add("read");
-    newUpdate.classList.add("update");
+  let newBook = document.createElement("div");
+  let newAuthor = document.createElement("p");
+  let newTitle = document.createElement("p");
+  let newMidSection = document.createElement("div");
+  let newPages = document.createElement("p");
+  let newRead = document.createElement("div");
+  let newUpdate = document.createElement("div");
 
-    cardContainer.appendChild(newBook);
-    newBook.append(newAuthor, newTitle, newMidSection, newUpdate);
-    newMidSection.append(newPages, newRead);
+  newBook.classList.add("bookCard");
+  newAuthor.classList.add("author");
+  newTitle.classList.add("title");
+  newMidSection.classList.add("midSection");
+  newPages.classList.add("pages");
+  newRead.classList.add("read");
+  newUpdate.classList.add("update");
 
-    newRead.textContent = "Read Y/N?";
-    newUpdate.textContent = "Update";
-    newAuthor.textContent = myLibrary[numOfCards].author;
-    newTitle.textContent = myLibrary[numOfCards].title;
-    newPages.textContent = myLibrary[numOfCards].pages;
-    if (myLibrary[numOfCards].read == true){
-        newRead.classList.add("readTrue");
-    }
+  cardContainer.appendChild(newBook);
+  newBook.append(newAuthor, newTitle, newMidSection, newUpdate);
+  newMidSection.append(newPages, newRead);
 
-    numOfCards++;
+  newRead.textContent = "Read Y/N?";
+  newUpdate.textContent = "Update";
+  newAuthor.textContent = myLibrary[numOfCards].author;
+  newTitle.textContent = myLibrary[numOfCards].title;
+  newPages.textContent = myLibrary[numOfCards].pages;
+  if (myLibrary[numOfCards].read == true) {
+    newRead.classList.add("readTrue");
+  }
+
+  numOfCards++;
 }
 
-function decideAction(btnText){
-    if (btnText === 'Add' ? storeBook(form.elements[0].value, form.elements[1].value, form.elements[2].value, isChecked())
-     : updateParse());
+function decideAction(btnText) {
+  if (
+    btnText === "Add"
+      ? storeBook(
+          form.elements[0].value,
+          form.elements[1].value,
+          form.elements[2].value,
+          isChecked()
+        )
+      : updateParse()
+  ); 
 }
 
-function updateLoad(){
-    openAddForm();
-    document.getElemenyById('add').textContent = 'Update';
+function updateLoad(e) {
+  /*Open our Form, and select the index where our HTML reflects our stored object.
+    Then display the whole object in our Form, ready for editing.*/
+  openAddForm();
+  updateIndex(e.target.parentElement.querySelector(".title").textContent);
+  document.getElementById("add").textContent = "Update";
+  form.elements[0].value = myLibrary[indexNumber].author;
+  form.elements[1].value = myLibrary[indexNumber].title;
+  form.elements[2].value = myLibrary[indexNumber].pages;
+  if (myLibrary[indexNumber].read == true) {
+    document.getElementById("readCheck").checked = true;
+  }
 }
 
-function updateParse(){
+function updateParse() {
+    myLibrary[indexNumber].author = form.elements[0].value;
+    myLibrary[indexNumber].title = form.elements[1].value;
+    myLibrary[indexNumber].pages = form.elements[2].value;
+    myLibrary[indexNumber].read = isChecked();
     closeForm();
+    resetForm();
+    indexNumber = undefined;
 }
 
+//Used to find the index number of targetted Cards for Updating their contents
+let updateIndex = function (title) {
+  indexNumber = myLibrary.findIndex((obj) => {
+    return obj.title === title;
+  }); //Seek Permanent Storage
+};
 
-
-
-
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("update")) updateLoad(e);
+});
 
 //Edit CSS when clicked
-window.addEventListener('click', (e) => { if (e.target.classList.contains('read')) e.target.classList.toggle("readTrue"); })
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("read"))
+    e.target.classList.toggle("readTrue");
+});
 
 //Cheaty method of including HTML5 Form Validation and DOM HTML attribute retrieval of form element data as function parameters.
 const form = document.querySelector(".formContainer");
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    decideAction(document.getElementById('add').textContent);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  decideAction(document.getElementById("add").textContent);
 });
 
-
 createCard();
-
-
-
-
-
-
 
 //Deprecated Anonymous function that adjusted CSS based on if my default book has been read or not, used during construction.
 /*(() => {
